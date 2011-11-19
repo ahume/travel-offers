e04e0b9fd3ca362aa546e3371425c1b7be2fa4e5
Grant <grant@grant-Dell-System-XPS-L702X.(none)>
2011-11-19 10:06:46 +0000

    Fixed INTCMP on search

--- src/main/scala/travel/offers/snippet/TravelOffers.scala
+++ src/main/scala/travel/offers/snippet/TravelOffers.scala
@@ -2,27 +2,27 @@ package travel.offers.snippet
 
 import net.liftweb.util._
 import Helpers._
-import travel.offers.Scoped
+import travel.offers.Scoped._
 import xml.Unparsed
 
 class TravelOffers {
 
-  def color = ".travel-offers [class+]" #> Scoped.backgroundColor.get
+  def color = ".travel-offers [class+]" #> backgroundColor.get
 
-  def links = ".browse-all [href]" #> ("http://www.guardianholidayoffers.co.uk?INTCMP=" + Scoped.campaign.get) &
-    ".email-signup [href]" #> ("http://www.guardianholidayoffers.co.uk/email-sign-up?INTCMP=" + Scoped.campaign.get)
+  def links = ".browse-all [href]" #> ("http://www.guardianholidayoffers.co.uk?INTCMP=" + campaign.get) &
+    ".email-signup [href]" #> ("http://www.guardianholidayoffers.co.uk/email-sign-up?INTCMP=" + campaign.get)
     ".browse-all [class]" #> "" &
     ".email-signup [class]" #> ""
 
 
   def search = {
-    val offer = Scoped.offers.get.head
+    val offer = offers.get.head
     ("#travel-offer [value]" #> (offer.keywords.headOption map { _.name } getOrElse (""))) &
-    (".travel-offers-search [action]" #> ("http://www.guardianholidayoffers.co.uk/Search?INTCMP=" + Scoped.campaign.get))
+    ("name=INTCMP [value]" #> campaign.get)
   }
 
   def trail = {
-    "*" #> (Scoped.offers.get.zip(trailClasses) map { case(offer, trailClass) =>
+    "*" #> (offers.get.zip(trailClasses) map { case(offer, trailClass) =>
       (".trail [class]"      #> trailClass) &
       (".offer-image [src]"  #> offer.imageUrl) &
       (".link [href]"        #> offer.offerUrl) &
@@ -33,12 +33,12 @@ class TravelOffers {
     })
   }
 
-  private def trailClasses = Scoped.numTrails.get match {
+  private def trailClasses = numTrails.get match {
     case 1 => List("")
     case 2 => List("two-col", "two-col edge")
   }
 
-  private def imageWidthInPixels = Scoped.imageSize.get match {
+  private def imageWidthInPixels = imageSize.get match {
     case "TwoColumn" => "280px"
     case "ThumbOne" =>  "140px"
   }
