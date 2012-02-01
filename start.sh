9ac6c9d6bd7afda15beea5ddb20eb42191691d93
Grant Klopper <grant.klopper@guardian.co.uk>
2012-02-01 15:51:45 +0000

    Removing proxy server from sbt config

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $APP_DIR/dev/environment.sh
 
 java -Xmx768M -XX:+UseCompressedOops -XX:MaxPermSize=250m \
-    -Dhttp.proxyHost=devscreen.gudev.gnl -Dhttp.proxyPort=3128 \
     -jar $APP_DIR/dev/sbt-launch-0.11.2.jar  "$@"
 
 
